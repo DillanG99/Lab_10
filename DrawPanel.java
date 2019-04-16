30db25c123af28630cfa6c5ad7574685af9d3e52
Dillan Gibbons <Dillan.G.Gibbons-1@ou.edu>
2019-04-16 15:51:22 -0500

    Completed the addShape() method

---
+++ DrawPanel.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class DrawPanel {
+	ArrayList<Shape> shapeList = new ArrayList<Shape>();
+	
+	public void addShape(Shape shape) {
+		shapeList.add(shape);
+	}
+	
+	public void paintComponent() {
+		
+	}
+}
